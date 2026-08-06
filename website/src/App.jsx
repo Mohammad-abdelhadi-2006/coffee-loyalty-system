5d7658d153e211453213735b3fbc4a45f4d8af80
Samer Alkhalili <sameralkhalili500@gmail.com>
2026-08-06 08:37:52 +0300

    Completely restructuring

---
+++ website/src/App.jsx
@@ -0,0 +1,14 @@
+import {Header , Footer} from './headerFooter.jsx'
+import Hero from './home.jsx'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ website/src/home.jsx
@@ -0,0 +1,11 @@
+function Hero() {
+  return (
+    <section id="hero">
+      <h1>قهوة طازجة لكل مزاج</h1>
+      <p>نكهات غنية وأجواء دافئة كل يوم.</p>
+      <button>اطلب الآن</button>
+    </section>
+  )
+}
+
+export default Hero
